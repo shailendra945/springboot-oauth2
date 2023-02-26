21793ae2014fc002fe5f81eef539fc39f4e2b5aa
shailendra945 <yadav.shailendra945@gmail.com>
2023-02-26 16:32:09 +0530

    refactor the class name properly

--- oauth-code-oauth2-server/src/main/java/com.infoiv/AuthorizationCodeOauth2Application.java
+++ oauth-code-oauth2-server/src/main/java/com.infoiv/AuthorizationCodeOauth2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootOauth2Application {
+public class AuthorizationCodeOauth2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootOauth2Application.class,args);
+        SpringApplication.run(AuthorizationCodeOauth2Application.class,args);
     }
 
 }
